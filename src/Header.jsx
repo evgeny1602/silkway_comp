9c40758ce30e66199edf911c9c630fa1c28d75b2
Evgeny Lobanov <evgeny1602@gmail.com>
2025-01-21 16:47:02 +0700

    Refactoring

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,7 +12,12 @@ export function HeaderLogo({ storeName }) {
 
   return (
     <a href="/">
-      <img className="min-w-[85px] h-[40px] header-4:w-[220px] header-4:h-[104px]" src={headerLogoImg} loading="lazy" alt={storeName} />
+      <img
+        className="min-w-[85px] h-[40px] header-4:w-[220px] header-4:h-[104px]"
+        src={headerLogoImg}
+        loading="lazy"
+        alt={storeName}
+      />
     </a>    
   )
 }
@@ -35,7 +40,9 @@ export function TopMenuItem({ item }) {
 export function TopMenu({ items }) {
 
   return (
-    <ul className="hidden header-4:flex gap-x-[30px] font-sans text-white text-sm items-start">
+    <ul
+      className="hidden header-4:flex gap-x-[30px] font-sans text-white text-sm items-start"
+    >
       {items.map(
         item => <TopMenuItem key={item.url} item={item} />
       )}
@@ -60,6 +67,76 @@ export function CatalogButton({ url }) {
   )
 }
 
+export function SearchFormWrapper({ children }) {
+
+  return (
+    <div
+      className="has-[:focus]:bg-white/5 h-[36px] header-4:h-[48px] hidden header-8:flex items-center p-[2px] pr-[3px] header-4:p-[4px] gap-[5px] border rounded border-white/45 transition-all duration-200 w-[300px] header-3:w-[484px] header-7:w-[380px] relative"
+    >
+      {children}
+    </div>
+  )
+}
+
+export function SearchFormIcon() {
+
+  return (
+    <img
+      src={searchIconImg}
+      className="hidden box-content header-5:block w-[22px] h-[22px] pl-[4px] pr-[4px] header-4:pl-[8px] header-4:pr-[6px]"
+    />
+  )
+}
+
+export function SearchFormButton() {
+
+  return (
+    <button
+      className="bg-white/20 h-[30px] header-4:h-[38px] text-base text-white hover:bg-white/30 transiotion-all duration-200 px-[8px] header-5:px-[33px] rounded font-sans"
+    >
+      <SearchFormIconRight />
+      <span
+        className="hidden header-5:inline"
+      >
+        Найти
+      </span>
+    </button>
+  )
+}
+
+export function SearchFormIconRight() {
+
+  return (
+    <img
+      src={searchIconImg}
+      className="box-content block header-5:hidden w-[22px] h-[22px] pl-[4px] pr-[4px] header-4:pl-[8px] header-4:pr-[6px]"
+    />
+  )
+}
+
+export function SearchFormInput({value, onChange}) {
+
+  return (
+    <input
+      className="focus:bg-transparent px-[4px] header-5:p-0 outline-none font-sans h-[30px] header-4:h-[38px] text-base bg-silkway-green text-white transition-all duration-200 w-full"
+      value={value}
+      onChange={onChange}
+    />
+  )
+}
+
+export function SearchFormResults({ results }) {
+  if (results.length == 0) return null
+
+  return (
+    <div
+      className="bg-white rounded shadow-md p-[15px] z-10 absolute left-0 top-[55px]"
+    >
+      {JSON.stringify(results)}
+    </div>
+  )
+}
+
 export function SearchForm({ searchUrl, autocompleteUrl }) {
   const [query, setQuery] = useState('')
   const [results, setResults] = useState([])
@@ -67,9 +144,7 @@ export function SearchForm({ searchUrl, autocompleteUrl }) {
 
   useEffect(() => {
     const autocomplete = async q => {
-      if (q == '') {
-        return
-      }
+      if (q == '') return
 
       const body = new FormData()
       body.append('SEARCH_QUERY', q)
@@ -86,59 +161,59 @@ export function SearchForm({ searchUrl, autocompleteUrl }) {
   }, [debouncedQuery])
 
   return (
-    <div
-      className="has-[:focus]:bg-white/5 h-[36px] header-4:h-[48px] hidden header-8:flex items-center p-[2px] pr-[3px] header-4:p-[4px] gap-[5px] border rounded border-white/45 transition-all duration-200 w-[300px] header-3:w-[484px] header-7:w-[380px] relative"
-    >      
-      <img
-        src={searchIconImg}
-        className="hidden box-content header-5:block w-[22px] h-[22px] pl-[4px] pr-[4px] header-4:pl-[8px] header-4:pr-[6px]"
-      />
+    <SearchFormWrapper>      
+      <SearchFormIcon />
 
-      <input
-        className="focus:bg-transparent px-[4px] header-5:p-0 outline-none font-sans h-[30px] header-4:h-[38px] text-base bg-silkway-green text-white transition-all duration-200 w-full"
+      <SearchFormInput
         value={query}
         onChange={e => {setQuery(e.target.value)}}
       />
 
-      {results.length > 0 &&
-        <div
-          className="bg-white rounded shadow-md p-[15px] z-10 absolute left-0 top-[55px]"
-        >
-          {JSON.stringify(results)}
-        </div>
-      }
+      <SearchFormResults results={results} />
 
-      <button
-        className="bg-white/20 h-[30px] header-4:h-[38px] text-base text-white hover:bg-white/30 transiotion-all duration-200 px-[8px] header-5:px-[33px] rounded font-sans"
-      >
-        <img
-          src={searchIconImg}
-          className="box-content block header-5:hidden w-[22px] h-[22px] pl-[4px] pr-[4px] header-4:pl-[8px] header-4:pr-[6px]"
-        />
-        <span
-          className="hidden header-5:inline"
-        >
-          Найти
-        </span>
-      </button>
+      <SearchFormButton />
+    </SearchFormWrapper>
+  )
+}
+
+export function CitySelectWrapper({ children }) {
+
+  return (
+    <div
+      className="h-[20px] flex gap-[10px]"
+    >
+      {children}
     </div>
+  )
+}
+
+export function CitySelectCurrent({ city }) {
+
+  return (
+    <span className="font-sans text-sm font-semibold text-white">
+      Мой город: {city}
+    </span>
+  )
+}
+
+export function CitySelectButton() {
+
+  return (
+    <button
+      className="font-sans text-xs text-white w-[77px] h-[20px] rounded bg-white/20 hover:bg-white/30 transition-all duration-200"
+    >
+      Изменить
+    </button>
   )
 }
 
 export function CitySelect({ initVal }) {
 
   return (
-    <div className="h-[20px] flex gap-[10px]">
-      <span className="font-sans text-sm font-semibold text-white">
-        Мой город: {initVal}
-      </span>
-
-      <button
-        className="font-sans text-xs text-white w-[77px] h-[20px] rounded bg-white/20 hover:bg-white/30 transition-all duration-200"
-      >
-        Изменить
-      </button>
-    </div>
+    <CitySelectWrapper>
+      <CitySelectCurrent city={initVal} />
+      <CitySelectButton />
+    </CitySelectWrapper>
   )
 }
 
@@ -175,46 +250,78 @@ export function LoginAccountButton({ text, url }) {
   )
 }
 
-export function CartButton({ cartItems }) {
+export function CartButtonWrapper({ children }) {
+
+  return (
+    <button
+      className="bg-silkway-orange hover:bg-silkway-light-orange transition-all duration-200 text-silkway-dark-chocolate rounded px-[8px] header-4:px-[15px] py-[5px] header-4:py-[10px] items-center text-xs header-4:text-sm shadow-inner shadow-white/45 border border-silkway-dark-orange whitespace-nowrap flex flex-nowrap order-first header-4:order-2 h-[40px] header-4:h-[84px] leading-[1.2]"
+    >
+      {children}
+    </button>
+  )
+}
+
+export function CartIcon() {
+
+  return (
+    <img
+        src={cartIconImg}
+        className="w-[20px] h-[25px] header-4:w-[27px] header-4:h-[34px] box-content block py-[0px] header-2:py-[10px] header-2:pl-[4px] pl-[0px] header-2:mr-[10px] pr-[8px] header-2:pr-[15px] header-2:border-r-[1px] border-silkway-dark-orange"
+      />
+  )
+}
+
+export function CartTitle() {
+
+  return (
+    <>
+      <span className="hidden header-2:inline">
+        Моя корзина
+      </span>
+      <span className="header-2:hidden inline">
+        Корзина
+      </span>
+    </>
+  )
+}
+
+export function CartSum({amount}) {
+
+  return (
+    <span className="font-semibold">
+      {formatMoney(amount)} ₽
+    </span>
+  )
+}
+
+export function CartProductsCount({ productsCount }) {
   const postfixes = ['товар', 'товара', 'товаров']
+
+  return (
+    <span className="hidden header-2:inline">
+      {productsCount} {itemsCountPostfix(productsCount, postfixes)}
+    </span>
+  )
+}
+
+export function CartButton({ cartItems }) {
+  
 
   const cartSum = cartItems
     .map(item => item.quantity * item.price)
     .reduce((s, x) => s + x, 0)
 
   return (
-    <button
-      className="bg-silkway-orange hover:bg-silkway-light-orange transition-all duration-200 text-silkway-dark-chocolate rounded px-[8px] header-4:px-[15px] py-[5px] header-4:py-[10px] items-center text-xs header-4:text-sm shadow-inner shadow-white/45 border border-silkway-dark-orange whitespace-nowrap flex flex-nowrap order-first header-4:order-2 h-[40px] header-4:h-[84px] leading-[1.2]"
-    >
-      <img
-        src={cartIconImg}
-        className="w-[20px] h-[25px] header-4:w-[27px] header-4:h-[34px] box-content block py-[0px] header-2:py-[10px] header-2:pl-[4px] pl-[0px] header-2:mr-[10px] pr-[8px] header-2:pr-[15px] header-2:border-r-[1px] border-silkway-dark-orange"
-      />
-      <div
-        className="text-left"
-      >
-        <span
-          className="hidden header-2:inline"
-        >
-          Моя корзина
-        </span>
-        <span
-          className="header-2:hidden inline"
-        >
-          Корзина
-        </span>
+    <CartButtonWrapper>
+      <CartIcon />
+      <div className="text-left">
+        <CartTitle />
         <br />
-        <span className="font-semibold">
-          {formatMoney(cartSum)} ₽
-        </span>
+        <CartSum amount={cartSum} />
         <br />
-        <span
-          className="hidden header-2:inline"
-        >
-          {cartItems.length} {itemsCountPostfix(cartItems.length, postfixes)}
-        </span>
+        <CartProductsCount productsCount={cartItems.length} />
       </div>      
-    </button>
+    </CartButtonWrapper>
   )
 }
 
@@ -226,6 +333,70 @@ export function MenuButton() {
     >
       <img src={menuIconImg} />
     </button>
+  )
+}
+
+export function HeaderBackground({ children }) {
+
+  return (
+    <div className="w-full bg-silkway-green">
+      {children}
+    </div>
+  )
+}
+
+export function HeaderContainer({ children }) {
+
+  return (
+    <div
+      className="max-w-[1670px] h-[80px] header-4:h-[206px] m-auto flex flex-nowrap items-center justify-start gap-[5px] header-9:gap-[30px] p-[10px] header-4:p-[50px] px-[10px] header-9:px-[20px] header-5:px-[50px]"
+    >
+      {children}
+    </div>
+  )
+}
+
+export function HeaderCenterSection({ children }) {
+
+  return (
+    <div
+      className="flex flex-nowrap flex-col justify-center header-4:justify-between gap-[14px] h-full header-4:h-[82px] ml-auto header-4:ml-0"
+    >
+      {children}
+    </div>
+  )
+}
+
+export function HeaderCenterSubSection({ children }) {
+
+  return (
+    <div
+      className="flex flex-nowrap gap-[14px] ml-auto header-4:ml-0"
+    >
+      {children}
+    </div>
+  )
+}
+
+export function HeaderRightSection({ children }) {
+
+  return (
+    <div
+      className="flex-col h-[80px] justify-between ml-auto hidden header-1:flex"
+    >
+      {children}
+    </div>
+  )
+}
+
+export function HeaderRightButtonsSection({ children }) {
+
+  return (
+    <div
+      className="flex flex-nowrap gap-[10px] ml-auto header-1:ml-0"
+    >
+      {children}
+    </div>
   )
 }
 
@@ -247,36 +418,30 @@ export function Header() {
     } = window.headerData
 
     return (
-      <div className="w-full bg-silkway-green">
-        <div
-          className="max-w-[1670px] h-[80px] header-4:h-[206px] m-auto flex flex-nowrap items-center justify-start gap-[5px] header-9:gap-[30px] p-[10px] header-4:p-[50px] px-[10px] header-9:px-[20px] header-5:px-[50px]"
-        >
+      <HeaderBackground>
+        <HeaderContainer>
           <HeaderLogo storeName={storeName} />
 
-          <div className="flex flex-nowrap flex-col justify-center header-4:justify-between gap-[14px] h-full header-4:h-[82px] ml-auto header-4:ml-0">
+          <HeaderCenterSection>
             <TopMenu items={topMenuItems} />
 
-            <div className="flex flex-nowrap gap-[14px] ml-auto header-4:ml-0">
+            <HeaderCenterSubSection>
               <CatalogButton url={catalogUrl} />
 
               <SearchForm 
                 searchUrl={searchProductUrl}
                 autocompleteUrl={searchProductAutocompleteUrl}
               />
-            </div>
-          </div>
+            </HeaderCenterSubSection>
+          </HeaderCenterSection>
 
-          <div
-            className="flex-col h-[80px] justify-between ml-auto hidden header-1:flex"
-          >
+          <HeaderRightSection>
             <CitySelect initVal="Рубцовск" />
 
             <HeaderPhone phone={phone} />
-          </div>
+          </HeaderRightSection>
 
-          <div
-            className="flex flex-nowrap gap-[10px] ml-auto header-1:ml-0"
-          >
+          <HeaderRightButtonsSection>
             <LoginAccountButton
               url={isLoggedIn ? accountUrl : loginUrl}
               text={isLoggedIn ? firstName : 'Войти'}
@@ -285,8 +450,8 @@ export function Header() {
             <CartButton cartItems={cartItems} />
 
             <MenuButton />
-          </div>          
-        </div>
-      </div>
+          </HeaderRightButtonsSection>          
+        </HeaderContainer>
+      </HeaderBackground>
     )
 }
