38abca07f706d16de16b53aeba3f6a8c215202ef
Evgeny Lobanov <evgeny1602@gmail.com>
2025-01-31 17:55:53 +0700

    Small fix

--- src/Header.jsx
+++ src/Header.jsx
@@ -621,7 +621,7 @@ function CartItemProperties({ properties }) {
 
 function CartItemRightContainer({ children }) {
   return (
-    <div className="max-[540px]:justify-end max-[540px]:w-[270pc] max-[540px]:ml-0 max-[540px]:pr-0 ml-auto flex flex-nowrap justify-between gap-[25px] items-center pr-2">
+    <div className="max-[540px]:justify-end max-[540px]:w-[180px] max-[540px]:pr-0 ml-auto flex flex-nowrap justify-between gap-[25px] items-center pr-2">
       {children}
     </div>
   )
