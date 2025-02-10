47a45489cad2970b752769587c57248110bdd30a
Evgeny Lobanov <evgeny1602@gmail.com>
2025-02-10 12:08:57 +0700

    Refactoring

--- src/main.jsx
+++ src/main.jsx
@@ -2,38 +2,45 @@ import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import './index.css'
 
-const init = async () => {
-  let el
-
-  el = document.getElementById('new-design-header')
-  if (el) {
-    const { Header } = await import('./sections/Header')
-    createRoot(el).render(
-      <StrictMode>
-        <Header />
-      </StrictMode>
-    )
+const intHeader = async () => {
+  const el = document.getElementById('new-design-header')
+  if (!el) {
+    return
   }
-
-  el = document.getElementById('hero-section')
-  if (el) {
-    const { HeroSection } = await import('./sections/HeroSection')
-    createRoot(el).render(
-      <StrictMode>
-        <HeroSection />
-      </StrictMode>
-    )
-  }
-
-  el = document.getElementById('footer')
-  if (el) {
-    const { Footer } = await import('./sections/Footer')
-    createRoot(el).render(
-      <StrictMode>
-        <Footer />
-      </StrictMode>
-    )
-  }
+  const { Header } = await import('./sections/Header')
+  createRoot(el).render(
+    <StrictMode>
+      <Header />
+    </StrictMode>
+  )
 }
 
-init()
+const initHeroSection = async () => {
+  const el = document.getElementById('hero-section')
+  if (!el) {
+    return
+  }
+  const { HeroSection } = await import('./sections/HeroSection')
+  createRoot(el).render(
+    <StrictMode>
+      <HeroSection />
+    </StrictMode>
+  )
+}
+
+const initFooter = async () => {
+  const el = document.getElementById('footer')
+  if (!el) {
+    return
+  }
+  const { Footer } = await import('./sections/Footer')
+  createRoot(el).render(
+    <StrictMode>
+      <Footer />
+    </StrictMode>
+  )
+}
+
+intHeader()
+initHeroSection()
+initFooter()
