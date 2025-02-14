e4756f5af3d5dc21316882c4b8c5a6af79424121
Evgeny Lobanov <evgeny1602@gmail.com>
2025-02-14 19:56:41 +0700

    BestsellersSection

---
+++ src/sections/BestsellersSection.jsx
@@ -0,0 +1,10 @@
+import { SectionContainer } from '../ui/SectionContainer'
+import { SectionInnerContainer } from '../ui/SectionInnerContainer'
+
+export function BestsellersSection() {
+  return (
+    <SectionContainer className="pt-[50px] pb-[70px] max-[874px]:pt-[20px] max-[874px]:pb-[50px]">
+      <SectionInnerContainer className="relative"></SectionInnerContainer>
+    </SectionContainer>
+  )
+}
