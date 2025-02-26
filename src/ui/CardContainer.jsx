4ff8393c0d0029930d1a1fd0a85fa75a05f83502
Evgeny Lobanov <evgeny1602@gmail.com>
2025-02-26 12:14:47 +0700

    CardContainer fix

--- src/ui/CardContainer.jsx
+++ src/ui/CardContainer.jsx
@@ -1,6 +1,6 @@
 export function CardContainer({ onClick, children, className = null }) {
   let classes =
-    'font-sans rounded flex flex-nowrap flex-col relative gap-[10px] mb-[40px] cursor-pointer hover:scale-105 transition-all duration-1000'
+    'font-sans rounded flex flex-nowrap flex-col relative gap-[10px] mb-[40px] cursor-pointer hover:scale-105 transition-all duration-1000 min-w-[135px] max-w-[210px]'
 
   if (className) {
     classes += ' ' + className
