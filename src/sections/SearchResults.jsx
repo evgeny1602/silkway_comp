229c023fc0b3d9f249ef6c9f7c084893ac209119
Evgeny Lobanov <evgeny1602@gmail.com>
2025-02-28 14:15:20 +0700

    SearchResultsContainer small fix

--- src/sections/SearchResults.jsx
+++ src/sections/SearchResults.jsx
@@ -40,7 +40,7 @@ function SearchResultsLoader() {
 
 function SearchResultsContainer({ children }) {
   return (
-    <div className="flex flex-wrap justify-start gap-[30px] max-[700px]:gap-[10px] relative">
+    <div className="flex flex-wrap justify-start gap-[30px] max-[700px]:gap-[10px] relative pb-[70px]">
       {children}
     </div>
   )
