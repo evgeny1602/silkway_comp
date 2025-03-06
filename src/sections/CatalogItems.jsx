c4af930813ae60572c0712b054a9a44c6f901d25
Evgeny Lobanov <evgeny1602@gmail.com>
2025-03-06 17:35:28 +0700

    Small fix

--- src/sections/CatalogItems.jsx
+++ src/sections/CatalogItems.jsx
@@ -9,7 +9,7 @@ export function CatalogItems() {
   return (
     <SectionContainer>
       <SectionInnerContainer>
-        <div className="flex gap-[30px] max-[1190px]:flex-col">
+        <div className="flex flex-nowrap gap-[30px] max-[1190px]:flex-col">
           <div>
             <div className="w-[370px] max-[1190px]:w-full bg-white rounded p-[15px]">
               <AccordionFilters />
